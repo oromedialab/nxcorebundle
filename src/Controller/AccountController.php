8de9b547ba2687715bd199db5b35cde3dea60d65
Ibrahim Azhar <azhar@iarmar.com>
2023-01-08 15:22:18 +0530

    Basic Route Setup

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace OroMediaLab\NxCoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccountController extends AbstractController
+{
+    public function authenticate()
+    {
+        die('sssss');
+    }
+}
--- src/NxCoreBundle.php
+++ src/NxCoreBundle.php
@@ -2,9 +2,15 @@
 
 namespace OroMediaLab\NxCoreBundle;
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class NxCoreBundle extends AbstractBundle
 {
-    
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+        // $container->import('../config/routing.yaml');
+    }
 }
