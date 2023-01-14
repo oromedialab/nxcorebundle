eb7a2d912b1ebf5e9b2cb576a1366f6e19d21293
Ibrahim Azhar <azhar@iarmar.com>
2023-01-14 17:16:47 +0530

    Updated API Response Codes

--- src/Enum/ApiResponseCode.php
+++ src/Enum/ApiResponseCode.php
@@ -8,13 +8,17 @@ enum ApiResponseCode: string
 {
     case AUTH_INVALID_CREDENTIALS = 'auth_invalid_credentials';
     case AUTH_SUCCESSFUL = 'auth_successful';
+    case FETCH_SUCCESS = 'fetch_success';
+    case RESOURCE_CREATED = 'resource_created';
 
     public function httpStatusCode(): int
     {
         return match($this)
         {
             self::AUTH_INVALID_CREDENTIALS => JsonResponse::HTTP_UNAUTHORIZED,
-            self::AUTH_SUCCESSFUL => JsonResponse::HTTP_OK
+            self::AUTH_SUCCESSFUL => JsonResponse::HTTP_OK,
+            self::FETCH_SUCCESS => JsonResponse::HTTP_OK,
+            self::RESOURCE_CREATED => JsonResponse::HTTP_CREATED
         };
     }
 }
