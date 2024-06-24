9aec7344c31822a36d3e46dd5d9351340511515c
Ibrahim Azhar <ibrahim.azhar@oromedialab.com>
2024-06-24 16:18:41 +0530

    Issue fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,6 +17,7 @@ use OroMediaLab\NxCoreBundle\Repository\UserRepository;
 #[ORM\Table(name: "user")]
 #[ORM\InheritanceType('SINGLE_TABLE')]
 #[ORM\DiscriminatorColumn(name: 'discr', type: 'string')]
+#[ORM\DiscriminatorMap(['user' => 'User'])]
 class User implements UuidableInterface, TimestampableInterface, PasswordAuthenticatedUserInterface, UserInterface
 {
     use UuidableTrait;
@@ -29,6 +30,9 @@ class User implements UuidableInterface, TimestampableInterface, PasswordAuthent
 
     #[ORM\Column(type: 'string', length: 50, unique: true, nullable: false)]
     private string $username;
+
+    #[ORM\Column(type: 'string')]
+    protected $role = 'ROLE_USER';
 
     #[ORM\Column(length: 100, nullable: true)]
     private string $password;
