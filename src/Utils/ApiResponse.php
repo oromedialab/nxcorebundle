2d1d9545b291ae1c202c245b1677c69a37898a9d
Ibrahim Azhar <ibrahim.azhar@oromedialab.com>
2024-10-02 17:36:29 +0530

    Added support for message

--- src/Utils/ApiResponse.php
+++ src/Utils/ApiResponse.php
@@ -12,14 +12,19 @@ class ApiResponse extends JsonResponse
         parent::__construct(static::body($apiResponseCode, $payload), $apiResponseCode->httpStatusCode());
     }
 
-    public static function body(ApiResponseCode $apiResponseCode, array $payload = array())
+    public static function body(ApiResponseCode $apiResponseCode, array $payload = array(), array $options = [])
     {
         $now = new \DateTime();
-        return [
-            'response_code' => $apiResponseCode->value,
-            'http_status_code' => $apiResponseCode->httpStatusCode(),
-            'timestamp' => $now->format(\DateTime::RFC3339),
-            'payload' => $payload
-        ];
+        $result = [];
+        $result['response_code'] = $apiResponseCode->value;
+        $result['http_status_code'] = $apiResponseCode->httpStatusCode();
+        if (!empty($options['message'])) {
+            $result['message'] = $options['message'];
+        }
+        $result['timestamp'] = $now->format(\DateTime::RFC3339);
+        if (!empty($payload)) {
+            $result['payload'] = $payload;
+        }
+        return $result;
     }
 }
