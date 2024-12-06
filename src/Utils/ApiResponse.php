20e6196a879e56e21e5d17864e13cc64ccf678a5
Ibrahim Azhar <ibrahim.azhar@oromedialab.com>
2024-12-06 21:19:46 +0530

    Added option to api response

--- src/Utils/ApiResponse.php
+++ src/Utils/ApiResponse.php
@@ -7,19 +7,23 @@ use OroMediaLab\NxCoreBundle\Enum\ApiResponseCode;
 
 class ApiResponse extends JsonResponse
 {
-    public function __construct(ApiResponseCode $apiResponseCode, array $payload = [])
+    public function __construct(ApiResponseCode $apiResponseCode, array $payload = [], array $options = [])
     {
-        parent::__construct(static::body($apiResponseCode, $payload), $apiResponseCode->httpStatusCode());
+        parent::__construct(static::body($apiResponseCode, $payload, $options), $apiResponseCode->httpStatusCode());
     }
 
-    public static function body(ApiResponseCode $apiResponseCode, array $payload = array())
+    public static function body(ApiResponseCode $apiResponseCode, array $payload = array(), array $options = [])
     {
         $now = new \DateTime();
-        return [
+        $response = [
             'response_code' => $apiResponseCode->value,
             'http_status_code' => $apiResponseCode->httpStatusCode(),
             'timestamp' => $now->format(\DateTime::RFC3339),
             'payload' => $payload
         ];
+        if (!empty($options['message'])) {
+            $response['message'] = $options['message'];
+        }
+        return $response;
     }
 }
